68f9b57acec390d0b6841820cd6299fe950b2f86
Vacarciuc <ivacarciuc0@gmail.com>
2023-10-30 20:00:42 +0200

    test002

---
+++ src/main/java/com/example/test/securingweb/WebSecurityConfig.java
@@ -0,0 +1,73 @@
+package com.example.test.securingweb;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http
+            .authorizeRequests()
+                .antMatchers("/user/adduser").permitAll()//pentru pagina principala permitem pentru toti
+                .anyRequest().authenticated() //pentru alte pagini cerem autorizare
+            .and()
+                .formLogin()
+                .loginPage("/logare_.....")
+                .permitAll()
+            .and()
+                .logout()
+                .permitAll();
+    }
+
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService(){
+        UserDetails userDetails=
+                User.withDefaultPasswordEncoder()
+                        .username("user")
+                        .password("password")
+                        .roles("USER")
+                        .build();
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests((requests) -> requests
+//                        .requestMatchers("/").permitAll()//pentru pagina principala permitem pentru toti
+//                        .anyRequest().authenticated()//pentru alte pagini cerem autorizare
+//                )
+//                .formLogin((form) -> form
+//                        .loginPage("/login")
+//                        .permitAll()
+//                )
+//                .logout((logout) -> logout.permitAll());
+//
+//        return http.build();
+//    }
+
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        UserDetails user =
+//                User.withDefaultPasswordEncoder()
+//                        .username("user")
+//                        .password("password")
+//                        .roles("USER")
+//                        .build();
+//
+//        return new InMemoryUserDetailsManager(user);
+//    }
+}
